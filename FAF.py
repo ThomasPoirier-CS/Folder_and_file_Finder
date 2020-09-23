0f775d5e5f1d1dcc3d6399d853bf0f7c29ab1b0f
ThomasPoirier-CS <70718793+ThomasPoirier-CS@users.noreply.github.com>
2020-09-23 22:14:59 +0200

    Update FAF.py
    Explication du code

--- FAF.py
+++ FAF.py
@@ -11,7 +11,7 @@ parser.add_option("-s", "--see-code", dest="seecode")
 
 # On place action pour retirer la demande d argument obligatoire pour la valeur help
 parser.add_option("-h", "--help", dest="help", action="store_true", help="help")
-
+# On declare la concatenation -> voir la documentation officielle
 (options, args) = parser.parse_args()
 
 # Declaration des variables et affectation des options aux variables
@@ -20,10 +20,10 @@ wordlist = options.wordlist
 helps = options.help
 s = options.seecode
 
-# Fonction de base du code
+# On declare la fonction de base du code
 def initial():
     if(arg):
-        req = requests.get(arg).status_code
+        req = requests.get(arg).status_code # On recupere le code HTTP dans une variable
         if req == 200:
             print("[!] Testing your URL, Please wait...")
             time.sleep(2)
@@ -31,13 +31,13 @@ def initial():
             time.sleep(1)
             print("[!] Script is running, Press CTRL + C to stop\n")
             liste = open(wordlist, "r").readlines()
-            for word in liste:
+            for word in liste: # On test chaque mot dans la wordlist
                 word = word.strip('\n')
-                cmd = "%s%s" %(arg, word)
-                cde = requests.post(cmd).status_code
+                cmd = "%s%s" %(arg, word) # Concatenation de lURL et le mot de la wordlist
+                cde = requests.post(cmd).status_code # Envoi de la requete avec le mot de la wordlist
                 if cde == 200:
                     cde = str(cde)
-                    if s == "200" or not s:
+                    if s == "200" or not s: # Pour pouvoir afficher si largument "s" est manquant
                         print("[*] Match Found: ("+(word)+") | CODE:"+(cde))
                 elif cde == 401:
                     cde = str(cde)
@@ -71,13 +71,15 @@ def helpuser():
 # Debut du code
 try
     if __name__ == "__main__":
-        if(arg):
+        if(arg): # Arguments de base pour le fonctionnement du code
             initial()
-        if(helps):
+        if(helps): # Si lutilisateur passe en argument laide
             helpuser()
-        if not(arg):
+        if not(arg): # On test la condition si largument est manquant
             print("\n[!] Error script init ! Please check the manual with options -h or --help")
+# On releve les erreurs en lien avec les certificats            
 except requests.exceptions.SSLError:
      print("\n[!] Error with the SSL Certificate, It's appears doesn't match with the given domain...")
+# On releve les erreurs en lien avec la mauvaise formulation de lURL        
 except requests.exceptions.ConnectionError:
      print("\n[!] Connection Error : The URL it's invalid... check the '/' in your URL")
